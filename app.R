8056d7009251047de3840a39a9cd24bae578c517
Dimi <dimitrios_K@gmx.de>
2022-09-20 13:56:11 +0200

    added functions.r to the require

--- app.R
+++ app.R
@@ -5,6 +5,8 @@ library(shinyWidgets)
 
 source("./GUI_components/UI.R")
 source("./GUI_components/server.R")
+source("./GUI_components/functions.R")
+
 
 options(shiny.maxRequestSize=200*1024**2) 
 shinyApp(ui, server, options = list(launch.browser = TRUE))
